6752b71c18977dddb4ecd3670ae9b70a2c0a68b1
Sumit Kumar <Sumit.Kumar@belden.com>
2025-07-26 22:48:47 +0530

    Update button click handler in App component to increment count by 4

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-
+  return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 4)}>
           count is {count}
         </button>
         <p>
